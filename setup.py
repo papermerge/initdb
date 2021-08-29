bb0c0ea2aec3cb0c48ab8a3a7be590c792a55a3a
Eugen Ciur <eugen@papermerge.com>
2021-08-29 09:46:31 +0200

    add psycopg2 dependecy in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ import setuptools
 
 setuptools.setup(
     name="initdb",
-    version="0.1.0",
+    version="0.2.0",
     author='Eugen Ciur',
     author_email='eugen@papermerge.com',
     url='https://github.com/papermerge/initdb',
     description="Small package that creates a user"
     " and database owned by that user.",
     packages=setuptools.find_packages('src'),
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    install_requires=[
+        'psycopg2 == 2.9.1',
+    ],
 )
