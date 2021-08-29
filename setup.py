008677cbe73930e94e99fca604880688ab543ce6
Eugen Ciur <eugen@papermerge.com>
2021-08-29 15:15:43 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="initdb",
-    version="0.3.0",
+    version="0.4.0",
     author='Eugen Ciur',
     author_email='eugen@papermerge.com',
     url='https://github.com/papermerge/initdb',
